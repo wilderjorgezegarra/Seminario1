e61179b7328bdb185261e83f349e381b05f83e12
TemocheGomez <tgomezlenninbil@crece.uss.edu.pe>
2020-05-10 03:14:26 -0500

    se agregan formularios
    se agregaron formularios 1 2 3

---
+++ G5_VB/Form2.vb
@@ -0,0 +1,28 @@
+﻿Public Class Form2
+    Private Sub Label1_Click(sender As Object, e As EventArgs)
+
+
+
+
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        Form1.Show()
+        Me.Hide()
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Form3.Show()
+        Me.Hide()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Form1.Show()
+        Me.Hide()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Form3.Show()
+        Me.Hide()
+    End Sub
+End Class
---
+++ G5_VB/Form3.vb
@@ -0,0 +1,21 @@
+﻿Public Class Form3
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        Form1.Show()
+        Me.Hide()
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        Form2.Show()
+        Me.Hide()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Form1.Show()
+        Me.Hide()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Form2.Show()
+        Me.Hide()
+    End Sub
+End Class
