ad2a8e1e2f5a5778e3df79d1c95aa03847e9721c
kevin alonso cabrera sanchez <kacs271093@gmail.com>
2020-05-10 12:20:20 -0500

    iuyiuyiuy

--- G5_VB/Form3.Designer.vb
+++ G5_VB/Form3.Designer.vb
@@ -137,7 +137,7 @@ Partial Class Form3
         'CheckedListBox1
         '
         Me.CheckedListBox1.FormattingEnabled = True
-        Me.CheckedListBox1.Location = New System.Drawing.Point(77, 115)
+        Me.CheckedListBox1.Location = New System.Drawing.Point(68, 190)
         Me.CheckedListBox1.Margin = New System.Windows.Forms.Padding(2)
         Me.CheckedListBox1.Name = "CheckedListBox1"
         Me.CheckedListBox1.Size = New System.Drawing.Size(81, 64)
