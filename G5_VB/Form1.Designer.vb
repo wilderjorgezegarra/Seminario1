713cbba22359f670427d9c72c38f6adebd6fadf8
Wilder Jorge Zegarra <wilderjz@hotmail.com>
2020-05-13 16:45:05 -0500

    Miercoles 13.  16:44

--- G5_VB/Form1.Designer.vb
+++ G5_VB/Form1.Designer.vb
@@ -22,29 +22,66 @@ Partial Class Form1
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.Button1 = New System.Windows.Forms.Button()
+        Me.btn_Nuevo = New System.Windows.Forms.Button()
+        Me.btn_Editar = New System.Windows.Forms.Button()
+        Me.btn_Eliminar = New System.Windows.Forms.Button()
+        Me.dgv_Asignaturas = New System.Windows.Forms.DataGridView()
+        CType(Me.dgv_Asignaturas, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
-        'Button1
+        'btn_Nuevo
         '
-        Me.Button1.Location = New System.Drawing.Point(203, 76)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
-        Me.Button1.TabIndex = 0
-        Me.Button1.Text = "Button1"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.btn_Nuevo.Location = New System.Drawing.Point(41, 170)
+        Me.btn_Nuevo.Name = "btn_Nuevo"
+        Me.btn_Nuevo.Size = New System.Drawing.Size(75, 23)
+        Me.btn_Nuevo.TabIndex = 0
+        Me.btn_Nuevo.Text = "&Nuevo"
+        Me.btn_Nuevo.UseVisualStyleBackColor = True
+        '
+        'btn_Editar
+        '
+        Me.btn_Editar.Location = New System.Drawing.Point(132, 170)
+        Me.btn_Editar.Name = "btn_Editar"
+        Me.btn_Editar.Size = New System.Drawing.Size(75, 23)
+        Me.btn_Editar.TabIndex = 1
+        Me.btn_Editar.Text = "Editar"
+        Me.btn_Editar.UseVisualStyleBackColor = True
+        '
+        'btn_Eliminar
+        '
+        Me.btn_Eliminar.Location = New System.Drawing.Point(225, 170)
+        Me.btn_Eliminar.Name = "btn_Eliminar"
+        Me.btn_Eliminar.Size = New System.Drawing.Size(75, 23)
+        Me.btn_Eliminar.TabIndex = 2
+        Me.btn_Eliminar.Text = "Eliminar"
+        Me.btn_Eliminar.UseVisualStyleBackColor = True
+        '
+        'dgv_Asignaturas
+        '
+        Me.dgv_Asignaturas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgv_Asignaturas.Location = New System.Drawing.Point(12, 12)
+        Me.dgv_Asignaturas.Name = "dgv_Asignaturas"
+        Me.dgv_Asignaturas.Size = New System.Drawing.Size(327, 150)
+        Me.dgv_Asignaturas.TabIndex = 3
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(278, 209)
-        Me.Controls.Add(Me.Button1)
+        Me.ClientSize = New System.Drawing.Size(351, 202)
+        Me.Controls.Add(Me.dgv_Asignaturas)
+        Me.Controls.Add(Me.btn_Eliminar)
+        Me.Controls.Add(Me.btn_Editar)
+        Me.Controls.Add(Me.btn_Nuevo)
         Me.Name = "Form1"
         Me.Text = "Form1"
+        CType(Me.dgv_Asignaturas, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
 
-    Friend WithEvents Button1 As Button
+    Friend WithEvents btn_Nuevo As Button
+    Friend WithEvents btn_Editar As Button
+    Friend WithEvents btn_Eliminar As Button
+    Friend WithEvents dgv_Asignaturas As DataGridView
 End Class
